77571e1f10a79fdaec1705ab717b7b58f72879ef
tahayk3 <tahayk3@gmail.com>
2024-02-09 16:12:29 -0600

    Agregando navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,17 @@ import SignIn from './pages/SingIn';
 import SignUp from './pages/SingUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from "./componentes/Header";
 
 
 export default function App(){
   return(
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home></Home>}>asdas</Route>
         <Route path="/about" element={<About></About>}>asdas</Route>
-        <Route path="/signIn" element={<SignIn></SignIn>}>asdas</Route>
+        <Route path="/sign-in" element={<SignIn></SignIn>}>asdas</Route>
         <Route path="/signUp" element={<SignUp></SignUp>}>asdas</Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}>asdas</Route>
         <Route path="/projects" element={<Projects></Projects>}>asdas</Route>
